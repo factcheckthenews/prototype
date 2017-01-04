e3be4ccc1c6584f76446896f79d3e1ea5b65d84f
uncompiled <chris@uncompiled.org>
2017-01-03 21:43:32 -0500

    Add CORS headers

--- handler.js
+++ handler.js
@@ -5,11 +5,18 @@ const credibleSources = require('./data/credible');
 const notCredibleSources = require('./data/notCredible');
 
 module.exports.check = (event, context, callback) => {
+	const origin = (event.queryStringParameters) ? event.queryStringParameters.__amp_source_origin : '*.factcheckthe.news';
 	const articleUrl = (event.queryStringParameters) ? event.queryStringParameters.url : '';
 	const domain = url2Domain(articleUrl);
 
 	const response = {
 		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Credentials': true,
+			'Access-Control-Allow-Origin': origin,
+			'AMP-Access-Control-Allow-Source-Origin': origin,
+			'Access-Control-Expose-Headers': 'AMP-Access-Control-Allow-Source-Origin'
+		},
 		body: JSON.stringify({
 			url: articleUrl,
 			score: '¯\\_(ツ)_/¯',
