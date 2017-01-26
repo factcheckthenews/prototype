be2f8f94a6634bffd348276d7a717942a31927e8
uncompiled <chris@uncompiled.org>
2017-01-26 01:07:21 -0500

    Add slander terms

---
+++ ext/slander.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Source: Oxford New Monitor Corpus 2014
+// http://blog.oxforddictionaries.com/2014/11/political-insults/
+
+const slanderTerms = {
+	loon: 100,
+	twit: 100,
+	nitwit: 100,
+	trash: 90,
+	hippie: 90,
+	moron: 87,
+	hypocrite: 85,
+	fool: 85,
+	scum: 85,
+	elitist: 80,
+	troll: 80,
+	shill: 80,
+	crony: 80,
+	hack: 80,
+	lemming: 80,
+	loser: 70,
+	racist: 70,
+	idiot: 70,
+	radical: 50,
+	bigot: 50,
+	wacko: 50,
+	blowhard: 50,
+	fringe: 50,
+	lunatic: 40,
+	crazy: 40,
+	thug: 40,
+	nutjob: 40,
+	nut: 40,
+	zealot: 20,
+	ideologue: 20,
+	misogynist: 10,
+	extremist: 10,
+	obstructionist: 10,
+	fanatic: 10
+};
+
+/** @method contains
+ * @description Searches collection for a slander word
+ * @param {Array} collection - Collection to search
+ * @returns true or false if political slander term is present in collection
+ */
+function contains(collection) {
+	return collection.some(word => {
+		return (word in slanderTerms);
+	});
+}
+
+/** @method terms
+ * @description Fetches slander words used
+ * @param {Array} collection - Collection to search
+ * @returns array of slander terms used
+ */
+function terms(collection) {
+	return collection.filter(word => {
+		return (word in slanderTerms);
+	});
+}
+
+/** @method spectrumLabel
+ * @description Labels the slander term on the political spectrum
+ * @param {String} term - Term to query
+ * @returns 'liberal' or 'conservative' based on how the word is used
+ */
+function spectrumLabel(term) {
+	if (term in slanderTerms) {
+		return (slanderTerms[term] > 50) ? 'liberals' : 'conservatives';
+	}
+	return null;
+}
+
+module.exports = {
+	in: contains,
+	terms: terms,
+	label: spectrumLabel
+};
--- handler.js
+++ handler.js
@@ -4,7 +4,8 @@ const url = require('url');
 const Promise = require('bluebird');
 const openSources = require('./ext/open-sources');
 const webOfTrust = require('./ext/web-of-trust');
-const entityExtraction = require('./ext/entity-extraction');
+const extraction = require('./ext/extraction');
+const slander = require('./ext/slander');
 
 module.exports.check = (event, context, callback) => {
 	const origin = getOrigin(event.queryStringParameters);
@@ -13,10 +14,15 @@ module.exports.check = (event, context, callback) => {
 	// Get reputation from Web of Trust
 	const wotPromise = webOfTrust
 		.getReputation(event.stageVariables.WOT_API_KEY, articleUrl);
-	const articlePromise = entityExtraction.getArticle(articleUrl);
+	const articlePromise = extraction.getArticle(articleUrl);
 
 	Promise.all([wotPromise, articlePromise])
 		.spread(function (wotResponse, articleResponse) {
+			// Tokenize text content and check for slander terms.
+			const content = extraction.format(articleResponse);
+			const tokens = extraction.getTokens(content.text);
+			const slanderTerms = slander.terms(tokens);
+
 			const response = {
 				statusCode: 200,
 				headers: {
@@ -36,7 +42,12 @@ module.exports.check = (event, context, callback) => {
 						},
 						https: isHTTPS(articleUrl),
 						webOfTrust: webOfTrust.format(wotResponse),
-						content: articleResponse
+						slander: {
+							flag: slander.in(tokens),
+							keywords: slanderTerms.map(item => {
+								return {term: item, label: slander.label(item)};
+							})
+						}
 					}
 				})
 			};
