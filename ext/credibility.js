ca29a7189cce0c04aade3d697f857b5a4107587c
uncompiled <chris@uncompiled.org>
2017-02-17 09:01:36 -0500

    Fixes incorrect score

--- ext/credibility.js
+++ ext/credibility.js
@@ -9,7 +9,7 @@ function score(criteria) {
 	const deceptiveDomain = (criteria.deceptiveDomain) ? 0 : 10;
 	const capitalization = (criteria.capitalization.title.flag) ? 0 : 10;
 	const punctuation = (criteria.punctuation.flag) ? 0 : 10;
-	const slander = (criteria.slander.flag) ? 0 : 10;
+	const slander = (criteria.slander) ? 0 : 10;
 	const opensources = (criteria.opensourcesFlagged) ? 0 : 25;
 	const webOfTrust = criteria.webOfTrust.reputation * 0.25;
 
